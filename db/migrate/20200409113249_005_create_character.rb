639dee123eb9300ee76c37e55185ee1746b09543
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-04-09 11:57:40 +0000

    Automatically backed up by Learn

--- db/migrate/20200409113249_005_create_character.rb
+++ db/migrate/20200409113249_005_create_character.rb
@@ -1,4 +1,4 @@
-class 005CreateCharacter < ActiveRecord::Migration[5.2]
+class CreateCharacter < ActiveRecord::Migration[5.2]
   def change
     create_table :characters do |t|
       t.string :name 
