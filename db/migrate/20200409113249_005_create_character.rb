b028962eae18fb6f6b61c9c049ae84e146a53a73
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-04-09 11:58:32 +0000

    Automatically backed up by Learn

--- db/migrate/20200409113249_005_create_character.rb
+++ db/migrate/20200409113249_005_create_character.rb
@@ -2,8 +2,8 @@ class CreateCharacter < ActiveRecord::Migration[5.2]
   def change
     create_table :characters do |t|
       t.string :name 
-      t.string :actor_id 
-      t.string :show_id
+      t.integer :actor_id 
+      t.integer :show_id
   end
 end
 end
